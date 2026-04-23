195e4897481e51c02abfa1004076bfc01cdff59e
Henyer268 <henyermelendezvirguez@inedimacg.edu.co>
2026-04-23 11:54:14 -0500

    Add files via upload

---
+++ Proyecto/sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'drawvs-v1';
+const ARCHIVOS = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ARCHIVOS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
